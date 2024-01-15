397e775ac4a47cd258ecd4339af27d4ed97d446b
Erasmo Junior <enojr2604dev@gmail.com>
2024-01-14 21:14:12 -0300

    Create Script Resolução Desafio.sql

---
+++ Script Resolução Desafio.sql
@@ -0,0 +1,72 @@
+-- 1
+SELECT Nome,
+	   Ano 
+FROM Filmes
+
+-- 2
+SELECT Nome,
+	   Ano,
+	   Duracao
+from Filmes order by ano
+
+-- 3
+SELECT Nome,
+	   Ano,
+	   Duracao
+from Filmes where Nome = 'De Volta para o Futuro'
+
+-- 4
+SELECT Nome,
+	   Ano,
+	   Duracao
+from Filmes where Ano = 1997
+
+-- 5
+SELECT Nome,
+	   Ano,
+	   Duracao
+from Filmes where Ano > 2000
+
+-- 6
+SELECT Nome,
+	   Ano,
+	   Duracao
+from Filmes where Duracao > 100 AND Duracao < 150 order by Duracao
+
+-- 7
+SELECT Ano,
+	   COUNT(*) Quantidade
+from Filmes group by Ano order by sum(Filmes.Ano) desc
+
+-- 8
+SELECT *
+FROM Atores WHERE Genero = 'M'
+
+-- 9
+SELECT *
+FROM Atores WHERE Genero = 'F' order by PrimeiroNome
+
+-- 10
+SELECT Filmes.Nome,
+	   Generos.Genero
+FROM Filmes 
+INNER JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
+INNER JOIN Generos ON Generos.Id = FilmesGenero.IdGenero
+
+-- 11
+SELECT Filmes.Nome,
+	   Generos.Genero
+FROM Filmes 
+INNER JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
+INNER JOIN Generos ON Generos.Id = FilmesGenero.IdGenero
+WHERE Generos.Genero = 'Mistério'
+
+-- 12
+
+SELECT Filmes.Nome,
+	   Atores.PrimeiroNome,
+	   Atores.UltimoNome,
+	   ElencoFilme.Papel
+FROM Filmes 
+INNER JOIN ElencoFilme ON Filmes.Id = ElencoFilme.IdFilme 
+INNER JOIN Atores ON Atores.Id = ElencoFilme.IdAtor
